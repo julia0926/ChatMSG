d5b38feb0c63f169eb5f1ab50a258c9500bad898
julia0926 <kj980926@naver.com>
2023-04-26 14:18:50 +0900

    initial commit

---
+++ ChatMSG/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatMSG
+//
+//  Created by Julia on 2023/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
